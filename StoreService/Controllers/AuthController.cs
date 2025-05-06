b65ee57b9f970947d4de0917920856a72280897e
ChinhTC <assin2910@gmail.com>
2025-05-06 23:39:37 +0700

    feat: add auth controller

--- StoreService/Controllers/AuthController.cs
+++ StoreService/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DTOs.Auth;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StoreService.IServices.Auth;
 
 namespace StoreService.Controllers
 {
@@ -7,5 +9,49 @@ namespace StoreService.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private IAuthService authService;
+
+        public AuthController(IAuthService service)
+        {
+            this.authService = service;
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<IActionResult> SignIn([FromBody] DTOSignIn request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var respone = await authService.SignIn(request);
+            if (respone.IsSuccess)
+            {
+                return Ok(respone);
+            }
+            else
+            {
+                return BadRequest(respone);
+            }
+        }
+
+        [HttpPost("sign-up")]
+        public async Task<IActionResult> SignUp([FromBody] DTOSignUp request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var respone = await authService.SignUp(request);
+            if (respone.IsSuccess)
+            {
+                return Ok(respone);
+            }
+            else
+            {
+                return BadRequest(respone);
+            }
+        }
     }
 }
--- StoreService/Program.cs
+++ StoreService/Program.cs
@@ -1,11 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using StoreService.Data;
+using StoreService.IServices.Auth;
+using StoreService.Services.Auth;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<UserDBContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//// Get configuring JWT from appsettings.json
+//var jwtSettings = builder.Configuration.GetSection("Jwt");
+//var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
+
+//// Config Authentication with JWT
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer = true,
+//            ValidateAudience = true,
+//            ValidateLifetime = true,
+//            ValidateIssuerSigningKey = true,
+//            ValidIssuer = jwtSettings["Issuer"],
+//            ValidAudience = jwtSettings["Audience"],
+//            IssuerSigningKey = new SymmetricSecurityKey(key),
+//            ClockSkew = TimeSpan.Zero
+//        };
+//    });
+
+// Add configuring Authorization
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddScoped<IAuthService, AuthService>();   
 
 var app = builder.Build();
 
@@ -18,6 +54,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
